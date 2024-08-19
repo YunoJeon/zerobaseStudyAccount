b63c1ba3a20ae7866a5fc51da9b8e8e99dc1b80d
jeon-yuno <zero0577@naver.com>
2024-08-19 16:33:22 +0900

    zerobaseStudyAccount

---
+++ src/main/java/com/example/account/config/LocalRedisConfig.java
@@ -0,0 +1,34 @@
+package com.example.account.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Configuration
+public class LocalRedisConfig {
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() {
+        redisServer = new RedisServer(redisPort);
+        try {
+            redisServer.start();
+            System.out.println("Redis started on port " + redisPort);
+        } catch (Exception e) {
+            System.err.println("Error starting Redis server" + e.getMessage());
+        }
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
